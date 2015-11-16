44f50df1764fb5b36521895cff5a562d416e4cfe
CharlieCaruana <chumio431@gmail.com>
2015-11-16 22:51:18 +1100

    First app completed

---
+++ Assignment Xcode Starter/Lesson01/modalviewcontroller.swift
@@ -0,0 +1,17 @@
+//
+//  modalviewcontroller.swift
+//  Lesson01
+//
+//  Created by Charles Caruana on 16/11/2015.
+//  Copyright © 2015 General Assembly. All rights reserved.
+//
+
+import Cocoa
+
+class modalviewcontroller: UIViewController {
+    
+
+    @IBAction func onExit(sender: AnyObject) {
+        
+    }
+}
