a4ed72e4ee889d4db3dbd62c68c541aa9fd0a02e
CharlieCaruana <chumio431@gmail.com>
2015-12-26 13:47:02 +1100

    HomeWork #3 Completed

---
+++ Assessments/Homework 3/Lesson03/orangeView.swift
@@ -0,0 +1,16 @@
+//
+//  orangeView.swift
+//  Lesson03
+//
+//  Created by Charles Caruana on 26/12/2015.
+//  Copyright © 2015 General Assembly. All rights reserved.
+//
+
+import UIKit
+class orangeView: UIViewController {
+
+    @IBAction func dismissOrangeVc(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+}
